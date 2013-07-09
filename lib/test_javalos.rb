150a07c04ecf5171e38625dd011d77f88722689d
Jonathan Avalos <javalos@pernix-solutions.com>
2013-07-08 21:42:30 -0600

    First commit for Test Javalos

---
+++ lib/test_javalos.rb
@@ -0,0 +1,72 @@
+class TestJavalos
+
+  def self.run
+    init
+    run_tests
+    print_messages
+  end
+
+  def assert_equals object_1, object_2
+    increment_test_count
+    if are_equals? object_1, object_2
+      test_passes
+    else
+      assert_equals_fails object_1, object_2
+    end
+  end
+
+  private
+
+  def self.init
+    @@messages = Array.new
+    @@count_tests = 0
+  end
+
+  def self.run_tests
+    classes = ObjectSpace.each_object(Class).to_a
+    test_classes = classes.find_all{|item| item.name =~ /(.*)Test/ }
+    test_classes.each do |test_class|
+      instance = test_class.new
+      test_class.instance_methods.grep(/test/) { |method| instance.send(method)}
+    end
+  end
+
+  def self.print_messages
+    puts ""
+    if @@messages.length > 0
+      puts "\nFailures:\n"
+      @@messages.each_with_index do |message, index|
+        puts ""
+        puts (index + 1).to_s + ") " + message
+      end
+      puts ""
+    end
+    puts "#{@@count_tests} tests, #{@@count_tests - @@messages.length} passed, #{@@messages.length} failed"
+    puts ""
+  end
+
+  def increment_test_count
+    @@count_tests = @@count_tests + 1
+  end
+
+  def are_equals? object_1, object_2
+    (object_1 == object_2)
+  end
+
+  def test_passes
+    printf '.'
+  end
+
+  def assert_equals_fails object_1, object_2
+    printf 'F'
+    @@messages << assert_equals_fail_message(object_1, object_2)
+  end
+
+  def assert_equals_fail_message object_1, object_2
+    "Assertion failed.\n" +
+        "   Expected: #{object_1.nil? ? "nil" : object_1} \n" +
+        "   Got: #{object_2.nil? ? "nil" : object_2} \n" +
+        "in: #{caller[2]}\n"
+  end
+end
+
---
+++ test_javalos.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'test_javalos'
+  s.version     = '0.0.1'
+  s.date        = '2013-01-01'
+  s.summary     = "test_javalos"
+  s.description = "test_javalos"
+  s.authors     = ["Jonathan Avalos"]
+  s.email       = 'javalosmen@gmail.com'
+  s.files       = ["lib/test_javalos.rb"]
+  s.homepage    =
+    'http://rubygems.org/gems/test_javalos'
+end
